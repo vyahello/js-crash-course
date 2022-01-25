06a6b91dac780903603ec98bb0c7027cf3dab0ce
Volodymyr Yahello <vyahello@gmail.com>
2022-01-25 16:44:48 +0200

    Add JS fundamentals 02 skeleton

---
+++ 02/script.js
@@ -0,0 +1,7 @@
+'use strict'; // activate string mode, should be very first line
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive');
