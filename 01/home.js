cc337bf655e13669d618c119b4dfd1a648a32f16
Volodymyr Yahello <vyahello@gmail.com>
2022-01-25 16:31:43 +0200

    Add JS fundamentals examples

--- 01/home.js
+++ 01/home.js
@@ -1,4 +1,3 @@
-// value
 let country = 'Ukraine'
 let continent = 'Europe'
 let population = 45_000_000
@@ -7,7 +6,7 @@ console.log(country);
 console.log(continent);
 console.log(population);
 
-// data types 
 
-
-
+const bill = 275;
+const tip = bill <= 300 && bill >= 50 ? bill * .15 : bill * 0.5;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
--- 01/script.js
+++ 01/script.js
@@ -94,10 +94,110 @@ else {
     century = 21;
 }
 console.log(century);
-*/
 
-// types 
+
+// type conversion
 const year = '1991';
 // console.log(year + 18); // 199118
 console.log(Number(year) + 18);
+console.log(Number('String')); // NaN (not a number)
+console.log(String(23), 23);
+// js can convert something to int, string or boolean
 
+// type coersion
+console.log("I'm " + 23 + ' years old');
+console.log('23' - '10' - 3); // will be number
+console.log('23' * '2'); // will be number
+console.log('23' / '2'); // will be number
+console.log('23' + '10' + 3); // will be string
+
+
+let n = '1' + 1;
+n = n - 1;
+console.log(10);
+console.log(2 + 3 + 4 + '5'); // 95
+console.log('10' - '2' + '5'); // 85
+
+// boolean
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0)); // false
+console.log(Boolean(undefined)); // false
+console.log(Boolean('')); // false
+console.log(Boolean('JJ')); // true
+console.log(Boolean({})); // true
+
+let height;
+if (height) {
+    console.log('defined');
+}
+else {
+    console.log('sorry');
+}
+
+// quality operators
+const age = 18;
+// strict, dont perform type coersion
+if (age === 18) console.log('adult'); // if one if the wont need {}
+// loose == operator, perform type coersion
+if (age == '18') console.log('adult');
+
+const fav = prompt("What's your age?");
+console.log(fav);
+console.log(typeof fav);
+
+if (fav === 18) { // strict 18 number
+    console.log('Yay!')
+}
+else if (fav === 7) {
+    console.log('Good');
+}
+else {
+    console.log('neither 18 nor 7');
+}
+
+if (fav !== 18) console.log('Different');
+
+// logic
+const hasLicense = true;
+const hasVision = true;
+console.log(hasLicense && hasLicense);
+console.log(hasLicense || hasLicense);
+console.log(!hasLicense);
+
+// switch
+const day = 'monday';
+
+switch (day) { // day === 'monday'
+    case 'monday':
+        console.log('Plan course');
+        break
+    case 'tuesday':
+        console.log('Prepare theory');
+        break
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code');
+        break
+    default:
+        console.log('None');
+}
+
+// expression / ternary
+false && false && !false
+const age = 23;
+age >= 18 ? console.log('Yay') : console.log('Nah')
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
+const check = age >= 20 ? 'Yay' : 'Nah';
+console.log(check);
+
+*/
+
+// JS releases 
+// ECMAScript is the standart
+// 2009 ES5 is released 
+// 2015 ES6 - the biggest update to the language
+// ES2016, ES2017, ES2018 etc.
+// backword compatible 1997 code works with modern JS engine 2020
+// old features are never removed
+// ES5 is supported in all browsers, ES6+ supported in modern browsers
