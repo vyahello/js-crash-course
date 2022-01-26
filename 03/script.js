16dbeff41cdfed6937c2ac1bb0f549dcb0115d9f
Volodymyr Yahello <vyahello@gmail.com>
2022-01-26 18:48:08 +0200

    Add dev tooling

---
+++ 03/script.js
@@ -0,0 +1,9 @@
+'use sctrict';
+
+// install 'prettier', 'todo highlight', 'settings sync', 'image preview'
+// docs - https://prettier.io/docs/en/options.html
+// TODO
+const x = 23;
+const calcAge = birth => 2037 - birth;
+
+console.log(x);
