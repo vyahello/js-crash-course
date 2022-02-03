d98e7856251413ff1bf6a2b965742ac681059986
Volodymyr Yahello <vyahello@gmail.com>
2022-02-03 17:23:25 +0200

    Add registration logic doe django app

---
+++ django/16-django-basics/learning_users/basic_app/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from basic_app import views
+
+# SET THE NAMESPACE!
+app_name = 'basic_app'
+
+# Be careful setting the name to just /login use userlogin instead!
+urlpatterns=[
+    url(r'^register/$', views.register, name='register'),
+]
